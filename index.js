9512f11c558526d94b5e774e6335a18cde49d402
unknown <uiltongb@gmail.com>
2018-11-18 18:30:55 -0200

    index.js and style.css

---
+++ index.js
@@ -0,0 +1,6 @@
+function login(){
+var userEmail = document.getElementById("main-div")
+    var userPassword = document.getElementById("password_field").nodeValue;
+
+    window.alert(userEmail + " " + userPassword)
+}

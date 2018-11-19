90245660c66378ba6af4ea7345f5bd80f6a9cc7e
unknown <uiltongb@gmail.com>
2018-11-19 14:28:40 -0200

    Login and logout working

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-function login(){
-    var userEmail = document.getElementById("email_field").nodeValue;
-    var userPassword = document.getElementById("password_field").nodeValue;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
 
-    window.alert(userEmail + " " + userPassword)
+        var user = firebase.auth().currentUser;
+
+        if(user != null){
+
+            var email_id = user.email;
+            document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+        }
+    } else {
+      // No user is signed in.
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+
+    }
+  });
+
+function login(){
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    //window.alert(userEmail + " " + userPassword)
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        window.alert("Error: "+ errorMessage);
+      });
+}
+
+function logout(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
 }

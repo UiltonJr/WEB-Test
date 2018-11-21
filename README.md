d5827062ce85cef88c0c1a7f126d57ccc29cc327
unknown <uiltongb@gmail.com>
2018-11-21 17:19:52 -0200

    Added README

---
+++ README.md
@@ -0,0 +1,12 @@
+Firebase Login Web
+
+This project was created to practice my skills in WEB technologies, I'll be adding more functionality to learning.
+
+Files in the Project:
+
+-HTML
+-CSS
+-JS
+-firebase integration
+
+Download the file and run it in your system, make sure to go project Authentication and enable Auth with Google as well as Email, and whitelist your domain.
--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-        document.getElementById("user_div").style.display = "block";
-        document.getElementById("login_div").style.display = "none";
-        document.getElementById("register_div").style.display = "none";
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+    document.getElementById("register_div").style.display = "none";
 
-        var user = firebase.auth().currentUser;
+    var user = firebase.auth().currentUser;
 
-        if(user != null){
-            var email_id = user.email;
-            document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-        }
-    }else {
-      // No user is signed in.
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
-      document.getElementById("register_div").style.display = "none";
+    if (user != null) {
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
     }
+  } else {
+    // No user is signed in.
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+    document.getElementById("register_div").style.display = "none";
+  }
+});
+
+function login() {
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    window.alert("Error: " + errorMessage);
   });
-
-function login(){
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
-
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        window.alert("Error: "+ errorMessage);
-      });
 }
 
-function logout(){
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
+function logout() {
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+  }).catch(function (error) {
+    // An error happened.
+  });
 }
-function compregister(){
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+function register() {
+  //window.alert(document.getElementById("btz").value);
+  var nuserEmail = document.getElementById("email_field").value;
+  var nuserPass = document.getElementById("password_field").value;
+  firebase.auth().createUserWithEmailAndPassword(nuserEmail, nuserPass).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
   });
-
-}
-/*function register(){
-    document.getElementById("user_div").style.display = "none";
-    document.getElementById("login_div").style.display = "none";
-    document.getElementById("register_div").style.display = "block";
-}*/
+} 
